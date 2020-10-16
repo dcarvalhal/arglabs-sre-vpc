d924cc78f53887374592612602f6de9bb7223f77
Andre Ramoni <andre.ramoni@gmail.com>
2020-10-16 19:58:25 -0300

    .

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -58,5 +58,27 @@ resource "aws_security_group" "sg_allow_in" {
 }
 output "sg_allow_in_id" { value = aws_security_group.sg_allow_in.id }
 
-
+# Allow COMMON
+resource "aws_security_group" "sg_allow_common" {
+  name        = "${terraform.workspace}-vpc-allow-common"
+  description = "Allow common traffic"
+  vpc_id      = aws_vpc.vpc.id
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = merge(var.tags, {
+    Name           = "${terraform.workspace}-vpc-allow-common"
+    environment    = terraform.workspace
+  },)
+}
+output "sg_allow_common_id" { value = aws_security_group.sg_allow_common.id }
 
