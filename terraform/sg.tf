66830fbcc477b951efb75ee4fee6784cf5e55828
Andre Ramoni <andre.ramoni@gmail.com>
2020-10-20 22:57:50 -0300

    .

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -64,10 +64,10 @@ resource "aws_security_group" "sg_allow_common" {
   description = "Allow common traffic"
   vpc_id      = aws_vpc.vpc.id
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["192.168.0.0/24", "10.0.0.0/16" ]
   }
   egress {
     from_port   = 0
