1f76a4c854365a0797392b2e2bf0dc59bc91d288
Andre Ramoni <andre.ramoni@gmail.com>
2020-11-18 15:39:54 -0300

    .

--- terraform/keypair.tf
+++ terraform/keypair.tf
@@ -1,6 +1,10 @@
 resource "aws_key_pair" "keypair" {
   key_name   = data.terraform_remote_state._42.outputs.keypair_name
   public_key = file("~/.ssh/id_rsa.pub")
+  tags = merge(var.tags, {
+    Name           = data.terraform_remote_state._42.outputs.keypair_name
+    environment    = terraform.workspace
+  },)
 }
 output "key_name" { value = aws_key_pair.keypair.key_name }
 
