968a96e7e15f118cde5b96e364af473217213c04
Andre Ramoni <andre.ramoni@gmail.com>
2020-11-18 16:56:41 -0300

    .

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,7 +4,7 @@ variable "tags" {
     terraform      = "true"
     team           = "infra"
     squad          = "sre"
-    project        = "env-vpc"
-    repository     = "git@github.com:ARGLabs/arglabs-sre-env-vpc.git" # git config --get remote.origin.url
+    project        = "vpc"
+    repository     = "git@github.com:ARGLabs/arglabs-sre-vpc.git" 
   }
 }
